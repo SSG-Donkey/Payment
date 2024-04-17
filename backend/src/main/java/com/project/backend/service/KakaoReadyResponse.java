6bbda7494cdc714427c7463bb69bc4d34f0b0d31
gyoungun Lee <leegyoungun99@gmail.com>
2024-04-17 12:20:47 +0900

    결제정보 db insert 구현

--- backend/src/main/java/com/project/backend/service/KakaoReadyResponse.java
+++
@@ -1,9 +0,0 @@
-package com.project.backend.service;
-
-import java.util.Date;
-
-public class KakaoReadyResponse {
-    private String tid;
-    private String next_redirect_pc_url;
-    private Date created_at;
-}
